a447b7ce80c755a9466d97f9bb2e9370278e9972
Isa <isaiah.j.murray@gmail.com>
2021-11-16 13:19:32 -0500

    pp poo poo

--- PROActive/ViewController.swift
+++ PROActive/ViewController.swift
@@ -1,20 +1,34 @@
 //
 //  ViewController.swift
-//  PROActive
+   //  PROActive
+  //
+ //  Created by Isaiah Murray on 11/9/21.
 //
-//  Created by Isaiah Murray on 11/9/21.
-//
+
+
 
 import UIKit
 var name: String?? = "User"
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    @IBOutlet weak var categoryPicker: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var nameInput: UITextField!
+    
+    @IBAction func categoryPickerEnd(_ sender: Any) {
+    }
+    @IBAction func DateEditingFinish(_ sender: Any) {
+    }
+    @IBAction func EventNameEditingFinish(_ sender: Any) {
+    }
     
     
-    
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+    
 
 
 }
